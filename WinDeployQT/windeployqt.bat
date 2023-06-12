1e673c4f67a9731a91318eaf366ec918629c6837
Thomas van Gemert <tvg@di.ku.dk>
2023-06-12 17:08:23 +0200

    Moved QT Deploy files to output folder

--- WinDeployQT/windeployqt.bat
+++ WinDeployQT/windeployqt.bat
@@ -1,6 +1,6 @@
 cd "../Release/bin/x64/app/"
 
-C:/local/Qt/5.15.2/msvc2019_64/bin/windeployqt.exe --dir "../../../../WinDeployQT/qtdata" --libdir . --plugindir "../../../../WinDeployQT/plugins" --no-system-d3d-compiler --compiler-runtime --no-opengl-sw --qmldir "../../../../WinDeployQT/res/qml" --release "OpenVR-MotionCompensationOverlay.exe"
+C:/local/Qt/5.15.2/msvc2019_64/bin/windeployqt.exe --dir qtdata --libdir . --plugindir qtdata/plugins --no-system-d3d-compiler --compiler-runtime --no-opengl-sw --qmldir res/qml --release "OpenVR-MotionCompensationOverlay.exe"
 
 @REM Debug:
 @REM windeployqt.exe --dir qtdata --libdir . --plugindir qtdata/plugins --no-system-d3d-compiler --compiler-runtime --no-opengl-sw --debug  --qmldir res/qml OpenVR-MotionCompensationOverlay.exe
