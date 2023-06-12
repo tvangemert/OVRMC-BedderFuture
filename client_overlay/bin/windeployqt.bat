ef193329432278fb438b86c9e92b2050cc8133b8
Thomas van Gemert <tvg@di.ku.dk>
2023-06-12 16:46:06 +0200

    New commit with new .gitignore

--- client_overlay/bin/windeployqt.bat
+++
@@ -1,9 +0,0 @@
-
-cd win64
-
-C:\local\Qt\5.15.2\msvc2019_64\bin\windeployqt.exe --dir qtdata --libdir . --plugindir qtdata/plugins --no-system-d3d-compiler --compiler-runtime --no-opengl-sw --qmldir res/qml --release OpenVR-MotionCompensationOverlay.exe
-
-@C:\local\Qt\5.15.2\msvc2019_64\bin\windeployqt.exe --dir qtdata --libdir . --plugindir qtdata/plugins --qmldir res/qml OpenVR-MotionCompensationOverlay.exe --release
-
-@REM Debug:
-@REM windeployqt.exe --dir qtdata --libdir . --plugindir qtdata/plugins --no-system-d3d-compiler --compiler-runtime --no-opengl-sw --debug  --qmldir res/qml OpenVR-MotionCompensationOverlay.exe
